8278254c66085886c983b6b18049788539b1cd47
lemys <lemyskaman@gmail.com>
2015-01-15 09:14:55 -0430

    starting with the simpliest   a cell

--- js/game.js
+++ js/game.js
@@ -1,5 +1,37 @@
+// the cell 
+function Cell (status){
+	this.status = status || false ;
+
+}
+//its methods
+Cell.prototype ={
+	toggleStatus:function(){
+		if (this.status==false){
+			this.status=true;
+		}else{
+			this.status=false;
+		}
+		
+	},
+	logStatus:function(){
+		console.log("cell status :"+this.status );
+	}
+
+};
+
+
+
+
+
+
 $(document).ready(function() {
+
+var cell= new Cell();
+
+cell.logStatus();
+cell.toggleStatus();
+cell.logStatus();
 	
-console.log("game has started");
+
 
 });
