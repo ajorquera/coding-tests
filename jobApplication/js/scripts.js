9056b63a6f71d1548962b6768d62a077803b2a9a
Olvis Quintana <programacion42@megabyteca.com>
2015-04-07 14:15:06 -0430

    feature/TEST-7 crea usuario en hipchat y lo redirecciona al login de hipchat

--- jobApplication/js/scripts.js
+++ jobApplication/js/scripts.js
@@ -5,12 +5,16 @@ $(document).ready(function (){
         ajaxCalls,
         name,
         password,
+        passwordConfirmation,
+        conditions,
         TOKEN,
+        TOKEN2,
         email,
         surname,
         APIHost;
     
     TOKEN = 'ZKgMulsDSz7hg7rbW26dbwaRLDmApKPaDONSTuQT';
+    //TOKEN2 = 'nNf831F6ZaAmgQrUOmhKK4jZDvtPrDFRODVMSUrM';
     
     APIHost = 'https://api.hipchat.com';
     
@@ -34,11 +38,26 @@ $(document).ready(function (){
                 "email"          : email
             };
             
-            console.log("The user has been created %o", ajaxData)
+            console.log("The user has been created %o", ajaxData);
+
+            $.ajax({
+                method: 'POST',
+                url: APIHost + '/v2/user',
+                data: JSON.stringify(ajaxData), 
+                crossDomain: true
+            }).then(
+                function(data) {
+                   redirectToHipchat();
+                },
+                function(err) {
+                    if(err.status == 400) {
+                        showErrorMessages();
+                    }
+                });
             
             deferred = $.Deferred();
             
-            deferred.resolve(true);
+            //deferred.resolve(true);
             
             return deferred.promise;
             
@@ -113,23 +132,59 @@ $(document).ready(function (){
             });
         }
     };
+
+    function validateEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    };
+
+    function validatePassword(pass) {
+        var re = /\b\w{8,}\b/;
+        return re.test(pass);
+    };
+
+    function redirectToHipchat() {
+       window.location.href = "https://www.hipchat.com/sign_in";
+    };
+
+    function showErrorMessages(err) {
+        if(err == 1) {
+            $('#errorPanel').show();
+            setTimeout( "$('#errorPanel').hide();",5000);
+        }
+
+        if(err == 2) {
+            $('#emailExist').show();
+            setTimeout( "$('#emailExist').hide();",5000);
+        }
+    };
     
     submitForm = function () {
+
         name = $('#name').val();
         surname = $('#surname').val();
         email = $('#email').val();
         password = $('#password').val();
         passwordConfirmation = $('#passwrodConfirmation').val();
-        
-        if(isValidForm()) {
-            showErrorMessages();
-        }    
-        
-        ajaxCalls.createUser()
-            .then(ajaxCalls.createRoom())
-            .then(ajaxCalls.addMembers())
-            .then(redirectToHipchat());
+        conditions = $('#conditionss').prop('checked');
+
+        if(validateEmail(email) && validatePassword(password) && password === passwordConfirmation && name !=='' && surname !== '' && conditions) {
+            ajaxCalls.createUser();
+        }else {
+            showErrorMessages(1);
+        }
+      
+       // ajaxCalls.createUser()
+          //  .then(ajaxCalls.createRoom())
+          //  .then(ajaxCalls.addMembers())
+          //  .then(redirectToHipchat());
+
     };
+
+    $("#aplicationForm").submit(function(ev) {
+        ev.preventDefault();
+        submitForm();
+    });
     
     
 });
